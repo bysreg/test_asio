214b5513f59622a8a4090cf3e7ff00698102aa5a
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2016-04-19 13:52:00 -0400

    Add timer_nonblocking_multiple_argument_callback_example

--- main.cpp
+++ main.cpp
@@ -5,16 +5,17 @@
 
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/bind.hpp>
 
 using namespace std;
 
-void lambda_example() 
+void lambda_example()
 {
 	using namespace boost::lambda;
-    typedef std::istream_iterator<int> in;
+	typedef std::istream_iterator<int> in;
 
-    std::for_each(
-        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+	std::for_each(
+		in(std::cin), in(), std::cout << (boost::lambda::_1 * 3) << " " );
 }
 
 void timer_blocking_example()
@@ -22,7 +23,7 @@ void timer_blocking_example()
 	boost::asio::io_service io;
 	boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
 	t.wait();
-	
+
 	cout<<"hello world"<<endl;
 }
 
@@ -40,13 +41,35 @@ void timer_nonblocking_example()
 	io.run(); // need this call for non blocking
 }
 
+void callback_1(const boost::system::error_code&, 
+	boost::asio::deadline_timer* t, int* count)
+{
+	if(*count < 5)
+	{
+		cout<<*count<<endl;
+		++(*count);
 
+		t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
+		t->async_wait(boost::bind(callback_1, boost::asio::placeholders::error, t, count));
+	}
+}
+
+void timer_nonblocking_multiple_argument_callback_example()
+{
+	boost::asio::io_service io;
+	int count = 0;
+	boost::asio::deadline_timer t(io, boost::posix_time::seconds(1));
+	t.async_wait(boost::bind(callback_1, boost::asio::placeholders::error, &t, &count));
+
+	io.run();
+}
 
 int main()
 {
 	//lambda_example();
-	//timer_blocking_example();	
-	timer_nonblocking_example();
+	//timer_blocking_example();
+	//timer_nonblocking_example();
+	timer_nonblocking_multiple_argument_callback_example();
 	return 0;
 }
 
