9212ced358991347697d6801e5cf9286e62dca4a
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2016-04-18 21:56:35 -0400

    boost lambda example and boost asio timer blocking example

--- main.cpp
+++ main.cpp
@@ -3,11 +3,33 @@
 #include <iterator>
 #include <algorithm>
 
-int main()
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+using namespace std;
+
+void lambda_example() 
 {
-    using namespace boost::lambda;
+	using namespace boost::lambda;
     typedef std::istream_iterator<int> in;
 
     std::for_each(
         in(std::cin), in(), std::cout << (_1 * 3) << " " );
+}
+
+void timer_blocking_example()
+{
+	boost::asio::io_service io;
+	boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+	t.wait();
+	
+	cout<<"hello world"<<endl;
+}
+
+int main()
+{
+	//lambda_example();
+	timer_blocking_example();	
+	
+	return 0;
 }
